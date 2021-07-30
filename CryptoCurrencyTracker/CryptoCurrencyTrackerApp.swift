111414d3ae78b611ffe94eb0a0e45a8cf91f4c28
Sagar Ranipa <Sagaar.ranipa@gmail.com>
2021-07-30 10:44:16 -0300

    Initial Commit

---
+++ CryptoCurrencyTracker/CryptoCurrencyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyTrackerApp.swift
+//  CryptoCurrencyTracker
+//
+//  Created by Sagar patel on 2021-07-30.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
